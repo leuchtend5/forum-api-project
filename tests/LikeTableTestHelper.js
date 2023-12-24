6ff3063e88cbfdc87c93174289058fb843d75d3a
leuchtend5 <zxcmj1@gmail.com>
2023-12-24 16:55:16 +0800

    add likeTableTestHelper in tests

---
+++ tests/LikeTableTestHelper.js
@@ -0,0 +1,28 @@
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const LikeTableTestHelper = {
+  async addLike({ id = 'like-123', commentId = 'comment-123', owner = 'user-123' }) {
+    const query = {
+      text: 'INSERT INTO likes VALUES ($1, $2, $3)',
+      values: [id, commentId, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async getLikeInComment({ commentId = 'comment-123', owner = 'user-123' }) {
+    const query = {
+      text: 'SELECT * FROM likes WHERE id = $1 AND owner = $2',
+      values: [commentId, owner],
+    };
+
+    const result = await pool.query(query);
+    return result.rows[0];
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM likes WHERE 1=1');
+  },
+};
+
+module.exports = LikeTableTestHelper;
