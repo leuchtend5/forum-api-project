44acd1b4ad3e193c786c2f5e6b49ffc70eec50fd
leuchtend5 <zxcmj1@gmail.com>
2023-12-28 22:59:36 +0800

    add like comment feature test case

---
+++ src/Domains/likes/LikeRepository.js
@@ -0,0 +1,15 @@
+class LikeRepository {
+  async addLike(like) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteLike(like) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getLikeByCommentId(id) {
+    throw new Error('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = LikeRepository;
